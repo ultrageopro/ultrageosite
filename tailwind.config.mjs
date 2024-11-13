1972b7ad8e43c5f475c9c8c71e9cc606ec3b78d3
ultrageopro <dev@ultrageopro.ru>
2024-11-13 22:24:26 +0300

    feat: post filtration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			fontSize: {
+				'responsive-small': 'clamp(1rem, 1.5vh + 0.5vw, 1.5rem)',
+			},
 		},
 	},
 	plugins: [
